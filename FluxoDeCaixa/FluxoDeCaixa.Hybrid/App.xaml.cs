9bf623b99ec0bf90b0049515d3551d7b70826419
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2024-04-01 21:00:39 -0300

    Add Blazor Hybrid

---
+++ FluxoDeCaixa/FluxoDeCaixa.Hybrid/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FluxoDeCaixa.Hybrid;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ FluxoDeCaixa/FluxoDeCaixa.Hybrid/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FluxoDeCaixa.Hybrid;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
