4228d0901cc94d521aeb04cde14487cddcdc649a
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2024-04-21 14:30:47 -0300

    Add Generic Entity
    Add Generic entity for generic use in the project

---
+++ src/Core/Domain/Entities/GenericEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace FluxoDeCaixa.Mobile.Core.Domain.Entities;
+
+public class GenericEntity
+{
+    public object? Key { get; set; }
+    public string KeyString { get; set; } = string.Empty;
+    public long KeyLong { get; set; }
+    public object? Value { get; set; }
+    public string ValueSring { get; set; } = string.Empty;
+    public long ValueLong { get; set; }
+
+    public GenericEntity() { }
+
+    public GenericEntity(object key, object value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+}
