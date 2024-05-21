7b4ca2f4f65a6829708a35123fc2dc59f534029d
Vinicius Gasparello da Silva <61200874+vgdsilva@users.noreply.github.com>
2024-05-21 15:10:39 -0300

    Feature: CreateOperationCategory changes

--- src/Mobile/Features/OperationCategory/CreateOperationCategory.cs
+++ src/Mobile/Features/OperationCategory/CreateOperationCategory.cs
@@ -8,7 +8,40 @@ public static class CreateOperationCategory
 {
    public static void Execute(Domain.Entities.OperationCategory entity)
     {
-        Database database = Core.Configuration.Factory.GetDatabase();   
+        using (Database database = Core.Configuration.Factory.GetDatabase())
+        {
+            try
+            {
+                database.SQL = "SELECT * FROM OPERATIONCATEGORY WHERE OPERATIONCATEGORYID = " + entity.OperationCategoryID;
+                List<Domain.Entities.OperationCategory> itens = (database.QueryAsync<Domain.Entities.OperationCategory>()).Result;
+                if (itens.Count > 0)
+                {
+                    database.SQL = @"UPDATE OPERATIONCATEGORY 
+                                     SET DESCRIPTION = @DESCRIPTION, 
+                                         COLORHEX = @COLORHEX, 
+                                         FAICON = @FAICON 
+                                     WHERE OPERATIONCATEGORYID = @OPERATIONCATEGORYID";
+                }
+                else
+                {
+                    database.SQL = @"INSERT INTO OPERATIONCATEGORIES (OPERATIONCATEGORYID, DESCRIPTION, COLORHEX, FAICON) 
+                                    VALUES (@OPERATIONCATEGORYID, @DESCRIPTION, @COLORHEX, @FAICON)";
+                }
+
+                database.AddParameter("@OPERATIONCATEGORYID", entity.OperationCategoryID);
+                database.AddParameter("@DESCRIPTION", entity.Description);
+                database.AddParameter("@COLORHEX", entity.ColorHex);
+                database.AddParameter("@FAICON", entity.FAIcon);
+                
+
+                database.Execute();
+            }
+            catch ( Exception ex )
+            {
+
+                throw;
+            }
+        }
     }
 
     public static void Validate(Domain.Entities.OperationCategory entity)
