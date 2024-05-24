9cc156b7089292a50bed1fa970928f3e23d24a30
Vinicius Gasparello da Silva <61200874+vgdsilva@users.noreply.github.com>
2024-05-24 17:22:17 -0300

    Add Web Project

---
+++ src/web/FluxoDeCaixaPessoal/Controllers/api/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluxoDeCaixaPessoal.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public partial class ApiController : ControllerBase
+{
+
+}
