e5c2916a4605e900872944534f8fc594bea67adb
Vinicius Gasparello da Silva <61200874+vgdsilva@users.noreply.github.com>
2024-05-28 13:27:06 -0300

    Folders Ajusts && FirebaseHelper

---
+++ mobile/src/utils/FirebaseHelper.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ8CMUWgfIa_7DCGx7qYUc0jQQSnWmObk",
+    authDomain: "fluxo-de-caixa-pessoal.firebaseapp.com",
+    projectId: "fluxo-de-caixa-pessoal",
+    storageBucket: "fluxo-de-caixa-pessoal.appspot.com",
+    messagingSenderId: "163183400425",
+    appId: "1:163183400425:web:b84568b772c0da5fdba778",
+    measurementId: "G-WSR9G664DE"
+}
+
+const app = initializeApp(firebaseConfig)
