14043a6a5bb610c773bd3385a5d890113dc39666
Vinicius Gasparello da Silva <61200874+vgdsilva@users.noreply.github.com>
2024-06-17 22:17:49 -0300

    api: Add LoginController

---
+++ api/FluxoDeCaixa.API/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluxoDeCaixa.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    public LoginController()
+    {
+        
+    }
+
+
+}
