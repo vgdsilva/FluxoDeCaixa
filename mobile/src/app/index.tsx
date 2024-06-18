cf4f32959719a298bd7aac970e4b962d2a4d7011
Vinicius Gasparello da Silva <61200874+vgdsilva@users.noreply.github.com>
2024-06-17 22:27:28 -0300

    mobile: Changes Home e Onboarding Screen

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,24 +1,15 @@
 import React from 'react'; 
 import { View, Text, StyleSheet } from 'react-native';
 import { BaseScreen } from '../components/baseScreen';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { CollapseView } from '../components/Containers/CollapseView';
+import { Link } from 'expo-router';
 
-export default class HomeScreen extends BaseScreen {
+export default class OnboardingScreen extends BaseScreen {
 
     render(): React.ReactNode 
     {
       return (
         <View style={styles.container}>
-          <View style={styles.header}>
-            <Text style={{ fontSize: 16, color: 'white' }}>Bem vindo</Text>
-            <Ionicons name="settings-sharp" 
-                      size={24} 
-                      color="white" />
-          </View>
-          <View style={{ padding: 24, minHeight: 100, width: '100%'}}>
-            <CollapseView title='Sua conta'></CollapseView>
-          </View>
+            <Link href="/home">to home page</Link>
         </View>
       );
     }
@@ -30,12 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#121214',
     flexDirection: 'column'
-  },
-  header: {
-    height: 'auto',
-    flexDirection: 'row',
-    alignContent: 'center',
-    justifyContent: 'space-between',
-    padding: 24,
   }
 });
