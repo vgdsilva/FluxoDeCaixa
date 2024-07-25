ac6593e35d6cc30660fb6522e403bea5dc0ffb1f
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2024-07-25 13:46:38 -0300

    Changes in the home page
    Creation of the header

--- mobile_maui/Views/Shell/AppShell.xaml.cs
+++ mobile_maui/Views/Shell/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace FluxoDeCaixa.Mobile
+﻿namespace FluxoDeCaixa.Mobile;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
