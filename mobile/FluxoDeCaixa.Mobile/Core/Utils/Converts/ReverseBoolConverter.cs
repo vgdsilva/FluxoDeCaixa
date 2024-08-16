056ae7c4bb4a1a691f5e36d327f94d81c6da49af
Vinicius Gasparello da Silva <61200874+vgdsilva@users.noreply.github.com>
2024-08-16 11:07:49 -0300

    Ajustes pagina Onboarding

---
+++ mobile/FluxoDeCaixa.Mobile/Core/Utils/Converts/ReverseBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace FluxoDeCaixa.Mobile.Core.Utils.Converts;
+
+public class ReverseBoolConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if(value == null)
+            return false;
+
+        return !(bool) value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
