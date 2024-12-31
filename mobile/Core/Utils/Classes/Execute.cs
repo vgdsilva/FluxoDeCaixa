5501e7a12302946370f4976a68cb623d84d39261
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2024-12-31 16:26:16 -0300

    feat: Login Page and View Model changes

---
+++ mobile/Core/Utils/Classes/Execute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluxoDeCaixa.MAUI.Core.Utils.Classes
+{
+    public static class Execute
+    {
+        public static async Task Task(Func<Task> command, Func<Task> onErrorCommand = null, Func<Task> onSuccessCommand = null)
+        {
+			try
+			{
+                await command();
+			}
+			catch (Exception ex)
+			{
+                if (onErrorCommand != null)
+                    await onErrorCommand();
+			}
+            finally
+            {
+                if (onSuccessCommand != null)
+                    await onSuccessCommand();
+            }
+        }
+    }
+}
