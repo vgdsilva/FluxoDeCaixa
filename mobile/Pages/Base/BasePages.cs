c7bf56cb17dc87fc14be6be394cfb68913787cd7
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2025-01-02 16:12:15 -0300

    feat: add base page and view model

---
+++ mobile/Pages/Base/BasePages.cs
@@ -0,0 +1,20 @@
+﻿namespace FluxoDeCaixa.MAUI.Pages.Base;
+
+public partial class BasePages : ContentPage
+{
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        (BindingContext as BaseViewModels)?.Page_Load(this);
+    }
+}
+
+public abstract partial class BaseViewModels : ObservableObject
+{
+    public BaseViewModels() { }
+
+    public abstract void Page_Load(object sender);
+}
+
