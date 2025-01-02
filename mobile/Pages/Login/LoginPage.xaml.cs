4b96333ec6c748f679505362fa211fc1260cc935
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2025-01-02 16:12:26 -0300

    feat: add login viewModel

--- mobile/Pages/Login/LoginPage.xaml.cs
+++ mobile/Pages/Login/LoginPage.xaml.cs
@@ -1,3 +1,5 @@
+using FluxoDeCaixa.MAUI.Pages.Base;
+
 namespace FluxoDeCaixa.MAUI.Pages.Login;
 
 public partial class LoginPage : ContentPage
@@ -5,5 +7,15 @@ public partial class LoginPage : ContentPage
 	public LoginPage()
 	{
 		InitializeComponent();
+
+		BindingContext = new LoginViewModel();
 	}
+}
+
+public partial class LoginViewModel : BaseViewModels
+{
+    public override void Page_Load(object sender)
+    {
+
+    }
 }
