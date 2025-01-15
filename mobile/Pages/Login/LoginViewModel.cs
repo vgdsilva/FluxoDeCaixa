c6a5b6a2936a0c1543854bdae991f5e6517fdf17
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2025-01-15 20:05:23 -0300

    feat: add command from login to home page

--- mobile/Pages/Login/LoginViewModel.cs
+++ mobile/Pages/Login/LoginViewModel.cs
@@ -1,8 +1,20 @@
+using FluxoDeCaixa.MAUI.Core.Utils.Classes;
 using FluxoDeCaixa.MAUI.Pages.Base;
+using FluxoDeCaixa.MAUI.Pages.Shell;
+using FluxoDeCaixa.MAUI.Utils.Classes;
 
 namespace FluxoDeCaixa.MAUI.Pages.Login;
 
 public partial class LoginViewModel : BaseViewModels
 {
-    
+
+
+    [RelayCommand]
+    async Task SignInWithMicrosoft()
+    {
+        await Execute.Task(async () =>
+        {
+            NavigationUtils.SetMainPage(new AppShell());
+        });
+    }
 }
